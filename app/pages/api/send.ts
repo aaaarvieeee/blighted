1454ce99b6e0a4e3883496cbce3d83ce1047465a
aaaarvieeee <119615693+aaaarvieeee@users.noreply.github.com>
2023-12-07 00:49:21 -0700

    test again

---
+++ app/pages/api/send.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from '../../components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const data = await resend.emails.send({
+      from: 'blighted <info@blighted.art>',
+      to: 'sangalangarvie@gmail.com',
+      subject: 'Hello world',
+      react: EmailTemplate({
+        firstName: 'John',
+        lastName: '',
+        phone: '',
+        message: ''
+      }),
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
