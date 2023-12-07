847deac2aa97325a3d94823cebc13402dba1aa90
aaaarvieeee <119615693+aaaarvieeee@users.noreply.github.com>
2023-12-07 00:20:49 -0700

    test again

--- app/pages/api/send/send.js
+++ app/pages/api/send/send.js
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-import { EmailTemplate } from '/components/EmailTemplate'
+import { EmailTemplate } from 'app/components/EmailTemplate';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
@@ -8,17 +7,14 @@ export async function POST(request) {
   try {
     const body = await request.json();
     console.log(body);
-    const { firstName, lastName, email, message } = body;
+    const { firstName, lastName, PhoneNumber, email, message } = body;
     const data = await resend.emails.send({
       from: 'blighted <info@blighted.art>',
       to: email,
       subject: 'Confirmation Email',
-      react: EmailTemplate({ firstName: firstName, lastName: lastName, message: message }),
+      react: EmailTemplate({ firstName, lastName, PhoneNumber, message }),
     });
 
-    if(data === 'success') {
-      return NextResponse.json({ message: 'Email sent'});
-    }
     return Response.json(data);
   } catch (error) {
     return Response.json({ error });
